d84ead55b53866cf695e8e577e376aefd4ad0380
Hey... It's ruffiØ! <44903742+BlackDahlia313@users.noreply.github.com>
2023-07-13 23:04:17 -0400

    Update directus.config.js

--- directus.config.js
+++ directus.config.js
@@ -1,7 +1,7 @@
 
 module.exports = function (env) {
     return {
-        // Railway inputs
+        // User inputs from railway starter button
         ADMIN_EMAIL: env.ADMIN_EMAIL,
         ADMIN_PASSWORD: env.ADMIN_PASSWORD,
         KEY: env.KEY,
@@ -11,7 +11,7 @@ module.exports = function (env) {
         PORT: env.PORT,
 
         // Reference: https://docs.railway.app/develop/variables#railway-provided-variables
-        PUBLIC_URL: `http://0.0.0.0:${env.PORT}`,
+        PUBLIC_URL: `https://${env.RAILWAY_STATIC_URL}`,
 
         // Database variables from Railway PostgreSQL Plugin
         // Reference: https://docs.railway.app/plugins/postgresql
@@ -27,6 +27,7 @@ module.exports = function (env) {
         REDIS_PORT: env.REDISPORT,
         CACHE_ENABLED: env.CACHE_ENABLED,
         CACHE_STORE: 'redis',
+        REDIS_USERNAME: env.REDISUSER,
         REDIS_PASSWORD: env.REDISPASSWORD,
         STORAGE_LOCATIONS: "azure",
         STORAGE_AZURE_DRIVER: "azure",
